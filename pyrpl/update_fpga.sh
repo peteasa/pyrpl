3599c6d9eadcc681515c7d7e11b96a40337c059b
Peter Saunderson <peteasa@gmail.com>
2025-02-08 07:17:29 +0000

    added custom bitfile support (eco system dtbo)

--- pyrpl/update_fpga.sh
+++ pyrpl/update_fpga.sh
@@ -1,37 +1,66 @@
 #!/bin/sh
 
-## This script is WIP.  This version is the same as /opt/redpitaya/sbin/overlay.sh
-## so custom FPGA is not supported at present
+CUSTOMDEVICETREE=/opt/$1/fpga.dtbo
+CUSTOMFPGA=/opt/$1/fpga.bit.bin
 
 FPGAS=/opt/redpitaya/fpga
 MODEL=$(/opt/redpitaya/bin/monitor -f)
-
-# Assume serverbinfilename or use $2 for the serverbinfilename
-CUSTOMFPGA=/opt/pyrpl/fpga.bin
 
 if [ "$?" = "0" ]
 then
 sleep 0.5s
 
-rmdir /configfs/device-tree/overlays/Full 2> /dev/null
+FPGA_REGION=Full
+if [ "$#" -gt "3" ]
+then
+    FPGA_REGION=$4
+    rmdir /configfs/device-tree/overlays/$FPGA_REGION 2> /dev/null
+else
+    for f in /configfs/device-tree/overlays/*; do
+        # remove all existing overlay regions
+        if [ -d "$f" ]
+        then
+            rmdir $f 2> /dev/null
+        fi
+    done
+fi
+
+rm -f /tmp/update_fpga.txt 2> /dev/null
 rm -f /tmp/loaded_fpga.inf 2> /dev/null
 
 sleep 0.5s
-echo -n "Commit "
-awk 'NR==2 {print $2}' $FPGAS/$MODEL/$1/git_info.txt
-
+FPGA_INF=$1
+DEVICETREETOINSTALL=$FPGAS/$MODEL/$1/fpga.dtbo
 FPGATOINSTALL=$FPGAS/$MODEL/$1/fpga.bit.bin
-# FPGATOINSTALL=$CUSTOMFPGA
-
-/opt/redpitaya/bin/fpgautil -b $FPGATOINSTALL -o $FPGAS/$MODEL/$1/fpga.dtbo -n Full > /tmp/update_fpga.txt 2>&1
-# TODO replace the following and implement overlay and get device tree for custom fpga
-# /opt/redpitaya/bin/fpgautil -b $FPGATOINSTALL -f Full > /tmp/update_fpga.txt 2>&1
-
-if [ "$?" = '0' ]
+if [ "$#" -gt "1" ]
 then
-    md5sum $FPGATOINSTALL >> /tmp/update_fpga.txt
+    FPGA_INF=$1_$2
+    FPGATOINSTALL=$CUSTOMFPGA
+fi
+
+if [ "$#" -gt "2" ]
+then
+    FPGA_INF=$1_$2_$3
+    DEVICETREETOINSTALL=$CUSTOMDEVICETREE
+fi
+
+MD5SUM=$(md5sum $FPGATOINSTALL)
+
+if [ "$#" -eq "1" ]
+then
+    echo -n "Commit "
+    awk 'NR==2 {print $2}' $FPGAS/$MODEL/$1/git_info.txt
+else
+    echo "Custom FPGA md5sum: $MD5SUM"
+fi
+
+/opt/redpitaya/bin/fpgautil -b $FPGATOINSTALL -o $DEVICETREETOINSTALL -n $FPGA_REGION > /tmp/update_fpga.txt 2>&1
+
+if [ "$?" = "0" ]
+then
+    echo "FPGA md5sum: $MD5SUM" >> /tmp/update_fpga.txt
     date >> /tmp/update_fpga.txt
-    echo -n $1 > /tmp/loaded_fpga.inf
+    echo -n $FPGA_INF > /tmp/loaded_fpga.inf
     exit 0
 else
     rm -f /tmp/update_fpga.txt 2> /dev/null
